b2274a23f672d073ff5651cdfcde852fd6fc05c9
Ono Saito <onosaito0614@gmail.com>
2023-08-26 16:35:04 +0900

    changed index

--- app/features/config/index.js
+++ app/features/config/index.js
@@ -24,7 +24,7 @@ export default {
     /**
      * The default server URL of Jitsi Meet Deployment that will be used.
      */
-    defaultServerURL: 'https://meet.aisleschool.co.jp',
+    defaultServerURL: 'https://meet.aislesoft.co.jp',
 
     /**
      * The default server Timeout in seconds.
